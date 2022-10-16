c15cec31f1bca05d9802c6aad9751a3d25546a48
Sebbafresh <Ereon@live.com>
2022-10-16 21:01:11 +0200

    .

--- d-s/src/app/app.module.ts
+++ d-s/src/app/app.module.ts
@@ -5,6 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DanielComponentComponent } from './daniel-component/daniel-component.component';
 import { SebastianComponentComponent } from './sebastian-component/sebastian-component.component';
+import { NG_ENTITY_SERVICE_CONFIG } from '@datorama/akita-ng-entity-service';
+import { AkitaNgDevtools } from '@datorama/akita-ngdevtools';
+import { AkitaNgRouterStoreModule } from '@datorama/akita-ng-router-store';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -14,9 +18,11 @@ import { SebastianComponentComponent } from './sebastian-component/sebastian-com
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    environment.production ? [] : AkitaNgDevtools.forRoot(),
+    AkitaNgRouterStoreModule
   ],
-  providers: [],
+  providers: [{ provide: NG_ENTITY_SERVICE_CONFIG, useValue: { baseUrl: 'https://jsonplaceholder.typicode.com' }}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
